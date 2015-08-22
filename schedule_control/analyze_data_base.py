d456b08cff38cc9463dffc12bb8b1af382526a7f
h-mayorquin <h.mayorquin@gmail.com>
2015-08-22 23:14:06 +0200

    Analysis file now ready

--- schedule_control/analyze_data_base.py
+++ schedule_control/analyze_data_base.py
@@ -18,5 +18,17 @@ print 'Work ', db['Arrived Time'].mean()
 print 'Leave', db['Left Work Time'].mean()
 
 # Print the 30 days mean for the three quantities
+print '-----------------------'
+print 'Statistics for the last 30 days'
+n = 30
+print 'Wake Up ', db['Woke Up Time'][-n:].mean()
+print 'Work ', db['Arrived Time'][-n:].mean()
+print 'Leave', db['Left Work Time'][-n:].mean()
 
 # Print the 7 days mean for the three quantities
+print '-----------------------'
+print 'Statistics for the last seven days'
+n = 7
+print 'Wake Up ', db['Woke Up Time'][-n:].mean()
+print 'Work ', db['Arrived Time'][-n:].mean()
+print 'Leave', db['Left Work Time'][-n:].mean()
